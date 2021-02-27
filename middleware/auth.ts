cb2e16a6df0c453725690bf85097a42f82c31bfe
MichailShcherbakov <MichailShcherbakov@users.noreply.github.com>
2021-02-27 23:50:37 +0300

    Replaced `axios` with `fetch` in auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -7,17 +7,34 @@ export default ({ app, redirect, req }) => {
     ? req.headers['user-agent']
     : navigator.userAgent;
 
-  return axios
+  return fetch('http://localhost:8080/v1/api/auth/refresh-tokens', {
+    method: 'POST',
+    headers: {
+      Cookie: `jwt-token=${token}`,
+      'User-Agent': userAgent,
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  })
+    .then((data) => data.json())
+    .then(({ data }) => {
+      app.$cookies.set('jwt-token', data.refreshToken);
+      app.store.dispatch(SET_TOKENS_ACTION, {
+        accessToken: data.accessToken,
+        refreshToken: data.refreshToken,
+      });
+    })
+    .catch(() => redirect('/sign-in'));
+
+  /* return axios
     .post(
       '/auth/refresh-tokens',
       {},
       {
-        headers: process.server
-          ? {
-              Cookie: `jwt-token=${token}`,
-              'User-Agent': userAgent,
-            }
-          : {},
+        headers: {
+          Cookie: `jwt-token=${token}`,
+          'User-Agent': userAgent,
+        },
       }
     )
     .then(({ data: { data } }) => {
@@ -27,5 +44,5 @@ export default ({ app, redirect, req }) => {
         refreshToken: data.refreshToken,
       });
     })
-    .catch(() => redirect('/sign-in'));
+    .catch(() => redirect('/sign-in')); */
 };
